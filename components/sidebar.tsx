4ebf67b27f0a676ec0ce5bad7fd4b68f8978a126
atharv-av <atharvv333@gmail.com>
2023-09-30 00:39:03 +0530

    completed dashboard UI

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -13,6 +13,7 @@ import {
   Settings,
   VideoIcon,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({
   weight: "600",
@@ -43,7 +44,7 @@ const routes = [
     icon: VideoIcon,
     href: "/video",
     color: "text-orange-700",
-},
+  },
   {
     label: "Music Generation",
     icon: Music,
@@ -64,6 +65,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-3 flex-1">
@@ -82,7 +84,7 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
